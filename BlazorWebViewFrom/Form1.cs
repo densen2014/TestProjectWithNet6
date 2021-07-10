ae40f105931d244f8b284a66444961b3522cae36
Alex Chow <zhouchuanglin@gmail.com>
2021-07-10 15:28:41 +0200

    Add project files.

---
+++ BlazorWebViewFrom/Form1.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.AspNetCore.Components.WebView.WindowsForms;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace BlazorWebViewFrom
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            var serviceCollection = new ServiceCollection();
+            serviceCollection.AddBlazorWebView();
+            serviceCollection.AddBootstrapBlazor();
+            var blazor = new BlazorWebView()
+            {
+                Dock = DockStyle.Fill,
+                HostPage = "wwwroot/index.html",
+                Services = serviceCollection.BuildServiceProvider(),
+            };
+            blazor.RootComponents.Add<Counter>("#app");
+            Controls.Add(blazor);
+        }
+         
+    }
+}
