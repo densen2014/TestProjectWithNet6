1f806b6dd599c180a4d6567ef0feb3191f737ea4
Alex Chow <zhouchuanglin@gmail.com>
2021-07-10 16:48:52 +0200

    2

---
+++ BlazorApp1/Pages/Counter.razor.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BlazorApp1.Pages
+{
+    public partial class Counter
+    {
+        private int currentCount = 1110;
+
+        private void IncrementCount()
+        {
+            currentCount += 9;
+        }
+
+    }
+}
